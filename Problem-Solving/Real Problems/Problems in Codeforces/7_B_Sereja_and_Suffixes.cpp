dcd8cf2edd476d0019a823cb74819f312418fd1b
plabandas <turjoyplaban09@gmail.com>
2022-03-17 10:21:12 +0600

    DONE :: 368B Sereja and Suffixes

--- Problem-Solving/Real Problems/Problems in Codeforces/7_B_Sereja_and_Suffixes.cpp
+++ Problem-Solving/Real Problems/Problems in Codeforces/7_B_Sereja_and_Suffixes.cpp
@@ -4,32 +4,40 @@ using namespace std;
 int main()
 {
 
-    // 8 3
-    // 8 6 4 3 4 2 4 8
-    // 6 4 2
-    // Output 0 0 2
+    // 8 3  //টেস্ট কেচ
+    // 8 6 4 3 4 2 4 8  // নাম্বার গুলো
+    // 6 4 2  // এই ইন্ডেক্স থেকি কইটা ডিসটিঙ্ক এলিমিন্ট আছে তা দেখাতে হবে
     // Answer 3 4 5
 
     int value, test;
 
-    cin >> value >> test;
+    cin >> value >> test; //টেস্ট কেচ ইনপুট
 
-    int arr[value + 1];
+    int arr[value + 1], index[value + 1];
 
-    for (int i = 1; i <= value; i++)
+    for (int i = 1; i <= value; i++) // দ্বিতীয় লাইন ইনপুট নেওয়া হয়েছে
     {
         cin >> arr[i];
+    }
+
+    set<int> elements; // ৩য় লাইন থেকে ডিসটিঙ্ক এলিমেন্ট বের করে সেভ করা হয়েছে যা ইন্ডেক্স[] মধ্যে সেভ আছে
+    for (int i = value; i >= 1; i--)
+    {
+        elements.insert(arr[i]);
+        index[i] = elements.size();
     }
 
     for (int i = 1; i <= test; i++)
     {
         int inx;
         cin >> inx;
-        set<int> elements;
-        for (int i = inx; i <= value; i++)
-        {
-            elements.insert(arr[i]);
-        }
-        cout << elements.size() << endl;
+
+        // প্রতিটা কেস এর জন্নে এই কমেন্ট ব্লক টা রান হসচ্ছিলো জার জন্যে বেশি টাইম লাগসিল ; যার জন্যে বেশি টাইম লাগসিল ; ERROR : Time limit exceeded on test 11
+        // set<int> elements;
+        //  for (int i = inx; i <= value; i++)
+        //  {
+        //      elements.insert(arr[i]);
+        //  }
+        cout << index[inx] << endl;
     }
 }
